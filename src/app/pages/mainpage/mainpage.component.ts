f507675c9ae53706341319b2ab33554ca5300988
Muhammad Abdullah <mabdulah596@hotmail.com>
2019-09-14 18:20:49 +0500

    admin

--- src/app/pages/mainpage/mainpage.component.ts
+++ src/app/pages/mainpage/mainpage.component.ts
@@ -14,7 +14,8 @@ export class MainpageComponent implements OnInit {
 users;
 lotteries;
 q=1;
-p=1;
+p=1;weekLotteries;
+winner;
   ngOnInit() {
     this.getLotteries();
     this.getUsers();
@@ -34,9 +35,81 @@ getLotteries(){
     let data =item.payload.doc.data();
     let id =item.payload.doc.id;
     return{id,...data}
-  }))).subscribe(res=>{
+  }))).subscribe((res:any)=>{
   this.lotteries=res;
   console.log(this.lotteries);
+  var curr = new Date;
+  console.log(curr.getDate());
+  console.log(curr.getDay());
+  curr.setUTCHours(0,0,0,0)
+  var first = (curr.getDate() - curr.getDay())+4; // First day is the day of the month - the day of the week
+var last = first + 7; // last day is the first day + 6
+
+var firstday = new Date(curr.setDate(first));
+var lastday = new Date(curr.setDate(last));
+console.log(firstday);
+console.log(lastday);
+let a=res.filter(item=>{
+  var dummy=new Date(item.date)
+  console.log(dummy);
+
+return dummy>=firstday && dummy< lastday
+})
+console.log(a);
+this.weekLotteries=a;
+let now= new Date
+let today=now.getDay();
+if(today==6){
+  this.getWinners();
+}
+console.log(today);
   })
 }
+chooseWinner(){
+  var ran = this.weekLotteries[Math.floor(Math.random() * this.weekLotteries.length)];
+  console.log(ran);
+  this.api.getUser(ran.userId).subscribe(resp=>{
+    this.winner=resp;
+    let data={
+name:this.winner.name,
+email:this.winner.email,
+id:ran.userId,
+phoneNumber:this.winner.phoneNumber,
+points:this.winner.points,
+winTime:this.winner.win+1
+    }
+    console.log(this.winner);
+  })
+}
+getWinners(){
+  this.api.getWinners().pipe(map(list=>list.map(item=>{
+    let data =item.payload.doc.data();
+    let id =item.payload.doc.id;
+    return{id,...data}
+  }))).subscribe((res:any)=>{
+    var curr = new Date;
+  console.log(curr.getDate());
+  console.log(curr.getDay());
+  curr.setUTCHours(0,0,0,0)
+  var first = (curr.getDate() - curr.getDay())+4; // First day is the day of the month - the day of the week
+var last = first + 7; // last day is the first day + 6
+
+var firstday = new Date(curr.setDate(first));
+var lastday = new Date(curr.setDate(last));
+console.log(firstday);
+console.log(lastday);
+let a=res.filter(item=>{
+  var dummy=new Date(item.date)
+  console.log(dummy);
+
+return dummy>=firstday && dummy< lastday
+})
+if(a.length==0){
+  this.chooseWinner();
+}else{
+  console.log("winner is already Descides");
+}
+  })
+}
+
 }
--- src/app/sevices/api/api.service.ts
+++ src/app/sevices/api/api.service.ts
@@ -10,10 +10,22 @@ export class ApiService {
   getUsers(){
     return this.afs.collection('users').snapshotChanges();
   }
+  updateUser(id,data){
+    return this.afs.doc('users/'+id).update(data);
+  }
   getLotteries(){
   return  this.afs.collection('lotteries',ref=>ref.orderBy('date','desc')).snapshotChanges();
   }
   getSingleLottery(id){
     return this.afs.doc('lotteries/'+id).valueChanges();
+  }
+  getUser(id){
+    return this.afs.doc('users/'+id).valueChanges();
+  }
+  getWinners(){
+    return this.afs.collection('winners').snapshotChanges();
+  }
+  addWinner(data){
+    return this.afs.collection('winners').add(data);
   }
 }
